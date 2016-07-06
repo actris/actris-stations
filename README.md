5328b67f518c3c3690bf52cc207b87b64c8de8c0
Richard Olav Rud <ror@nilu.no>
2016-07-06 11:18:37 +0200

    added new strucutre and removed old files

--- README.md
+++
@@ -1,1 +0,0 @@
-# actris-stations
---
+++ existing_and_future.js
@@ -0,0 +1,84 @@
+var width = 1260,
+    height = 700;
+
+//tutorial for this map
+//http://www.d3noob.org/2013/03/a-simple-d3js-map-explained.html
+ 
+var projection = d3.geo.mercator()
+    .center([0, 60])
+    .scale(600)
+    .rotate([0,0]);
+
+var svg = d3.select("body").append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+var path = d3.geo.path()
+    .projection(projection);
+
+var g = svg.append("g");
+
+// load and display the World
+d3.json("world-110m2.json", function(error, topology) {
+
+// load and display the data
+d3.csv("existing_and_future_stations.csv", function(error, data) {
+
+
+
+    g.selectAll("circle")
+       .data(data)
+       .enter()
+       .append("a")
+	.attr("xlink:href", function(d) {
+			return "http://actris.nilu.no/";}
+	)
+       .append("circle")
+       .attr("cx", function(d) {
+               return projection([d.lon, d.lat])[0];
+       })
+       .attr("cy", function(d) {
+               return projection([d.lon, d.lat])[1];
+       })
+       .attr("r", 3)
+       .style("fill", function(d){
+
+       		if(d.network == "EMEP"){
+                  return "red";     
+            }else if(d.network == "ACTRIS-INSITU"){
+                  return "blue";     
+            }
+		
+		
+       })
+       .attr("opacity", ".6");
+    
+
+       
+
+
+
+});
+
+
+g.selectAll("path")
+      .data(topojson.object(topology, topology.objects.countries)
+          .geometries)
+    .enter()
+      .append("path")
+      .attr("d", path)
+});
+
+// zoom and pan
+var zoom = d3.behavior.zoom()
+    .on("zoom",function() {
+        g.attr("transform","translate("+ 
+            d3.event.translate.join(",")+")scale("+d3.event.scale+")");
+        g.selectAll("circle")
+            .attr("d", path.projection(projection));
+        g.selectAll("path")  
+            .attr("d", path.projection(projection)); 
+
+  });
+
+svg.call(zoom)
